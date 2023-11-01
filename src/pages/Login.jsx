681a5647e1724b54225ff52b4b7ba81c63961600
Jason <jasonchen5044@gmail.com>
2023-11-01 13:22:09 -0400

    Connected auth to forms, made auth global scoped

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,51 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
 
 export const Login = () => {
+  const [state, setState] = useState({ email: "", password: "", error: null });
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleFormChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      await login(state.email, state.password);
+      navigate("/");
+    } catch (e) {
+      //TODO: Create error alert
+      setState({ ...state, error: "Email or Password incorrect" });
+    }
+  };
+
   return (
     <div className="flex items-center justify-center flex-1">
-      <form action="" className="flex flex-col h-auto gap-8 p-6 shadow-lg w-96 rounded-2xl dark:shadow-white">
+      <form action="" className="flex flex-col h-auto gap-8 p-6 shadow-lg w-96 rounded-2xl dark:bg-neutral-800/10">
         <h2 className="mt-8 text-2xl font-medium text-center dark:text-white">Authenticate</h2>
 
         <div className="flex flex-col">
-          <label className="dark:text-white">Username</label>
-          <input type="text" className="form-input" required />
+          <label className="dark:text-white">Email</label>
+          <input type="text" name="email" className="form-input" onChange={handleFormChange} required />
         </div>
 
         <div className="flex flex-col">
           <label className="dark:text-white">Password</label>
-          <input type="password" className="form-input" required />
+          <input type="password" name="password" className="form-input" onChange={handleFormChange} required />
         </div>
 
-        <button type="submit" className="btn-primary">
+        <button type="submit" className="btn-primary" onClick={handleLogin}>
           Login
         </button>
 
         <p className="text-center dark:text-white">
-          First time? <Link to="/signup" className="hover:underline">Sign up</Link>
+          First time?{" "}
+          <Link to="/signup" className="hover:underline">
+            Sign up
+          </Link>
         </p>
       </form>
     </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,3 +1,74 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
 export const SignUp = () => {
-  return <p>Sign up page</p>;
+  const [state, setState] = useState({ email: "", password: "", confirmPassword: "", error: null });
+  const { guestLogin, signUp } = useAuth();
+  const navigate = useNavigate();
+
+  const handleGuestLogin = async () => {
+    try {
+      await guestLogin();
+      navigate("/");
+    } catch (e) {
+      console.error("Error with guest login");
+    }
+  };
+
+  const handleSignUp = async (event) => {
+    event.preventDefault();
+    if (state.password !== state.confirmPassword) {
+      //TODO: Create an error box 
+      setState({ ...state, error: "Different passwords" });
+    } else {
+      try {
+        await signUp(state.email, state.password);
+        navigate("/");
+      } catch (e) {
+        console.error("Error with signing up");
+      }
+    }
+  };
+
+  const handleFormChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className="flex items-center justify-center flex-1">
+      <form action="" className="flex flex-col h-auto gap-8 p-6 shadow-lg w-96 rounded-2xl dark:bg-neutral-800/10">
+        <h2 className="mt-8 text-2xl font-medium text-center dark:text-white">Sign Up</h2>
+
+        <div className="flex flex-col">
+          <label className="dark:text-white">Email</label>
+          <input type="email" name="email" className="form-input" onChange={handleFormChange} required />
+        </div>
+
+        <div className="flex flex-col">
+          <label className="dark:text-white">Password</label>
+          <input type="password" name="password" className="form-input" onChange={handleFormChange} required />
+        </div>
+
+        <div className="flex flex-col">
+          <label className="dark:text-white">Confirm Password</label>
+          <input type="password" name="confirmPassword" className="form-input" onChange={handleFormChange} required />
+        </div>
+
+        <button type="submit" className="btn-primary" onClick={handleSignUp}>
+          Sign Up
+        </button>
+
+        <div className="text-center dark:text-white">
+          First time?{" "}
+          <Link to="/signup" className="hover:underline">
+            Sign up
+          </Link>
+          <p className="mt-1 text-xs italic hover:underline hover:cursor-pointer" onClick={handleGuestLogin}>
+            Or continue as guest
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
